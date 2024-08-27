b5ae29991f561ece9b36f5826ef144d4cc4f2c33
shunta0213 <shuntaide16213@gmail.com>
2024-08-27 23:09:56 +0900

    base of graphql

---
+++ graph/model/todo.go
@@ -0,0 +1,9 @@
+package model
+
+type Todo struct {
+	ID     string `json:"id"`
+	Text   string `json:"text"`
+	Done   bool   `json:"done"`
+	User   *User  `json:"user"`
+	UserID string `json:"userId"`
+}
